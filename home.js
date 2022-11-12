cd5779338b28d89e08dd5698d58db0a45b0d1abd
Zahir <zahir.maliqi@studentet.uni-pr.edu>
2022-11-12 01:48:40 -0300

    Commit the main part

---
+++ home.js
@@ -0,0 +1,23 @@
+function showhideDetails() {
+    var x = document.getElementById("show-details");
+    var k= document.getElementById("experience-bttn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      za.textContent="Hide";
+    } else {
+      x.style.display = "none";
+      za.textContent="Show";
+    }
+  }
+
+  function showhideDetails1() {
+    var x = document.getElementById("show-details1");
+    var k= document.getElementById("experience-bttn1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      za.textContent="Hide";
+    } else {
+      x.style.display = "none";
+      za.textContent="Show";
+    }
+  }
